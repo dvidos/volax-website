3598aee1c560230bbef621b6baa6335cf3e411e7
Dimitris <Dimitris@dimitris4520s>
2014-02-10 22:03:19 +0200

    static pages now 600px narrow

--- protected/views/page/view.php
+++ protected/views/page/view.php
@@ -2,7 +2,7 @@
 $this->pageTitle=$model->title;
 ?>
 
-<div class="page">
+<div class="static-page">
 
 	<h1 class="title"><?php echo CHtml::encode($model->title); ?></h1>
 	<?php echo $model->content; ?>
