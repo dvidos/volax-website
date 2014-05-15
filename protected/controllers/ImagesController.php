115492c6c3841b43ce9fc3c7942fca758a839c72
Dimitris <Dimitris@dimitris4520s>
2014-05-15 20:48:56 +0300

    ImagesController now also takes maxwidth, maxheight, allowgrow as parameters

--- protected/controllers/ImagesController.php
+++ protected/controllers/ImagesController.php
@@ -10,38 +10,39 @@ class ImagesController extends Controller
 	 * If both dimensions are given, and are far from aspect ratio, 
 	 * the action tries to return a meaningful portion of the image.
 	 */
-	public function actionShow($src = '', $width = '', $height = '')
+	public function actionShow($src = '', $width = 0, $height = 0, $maxwidth = 0, $maxheight = 0, $allowgrow = 1)
 	{
+		// first see if a file is given
 		if ($src == '')
 			throw new CHttpException(400, 'No image SRC given');
 
-		// if nothing given, simply redirect to the file.
-		if (($width == '' && $height == '') || ($width == 0 && $height == 0))
-		{
-			Yii::log('No dimensions given, redirecting to original source: "' . $src . '"', 'info', 'ImagesController');
-			header('Location: ' . $src);
-			return;
-		}
-		
-		// usually, spaces will be rendered as %20. try to decode here...
 		$originalFilename = urldecode($src);
 		if ($originalFilename != $src)
 			Yii::log('Original filename for url "' . $src . '" is "' . $originalFilename . '"', 'info', 'ImagesController');
-			
 		
 		if (!file_exists($originalFilename))
 			throw new CHttpException('File "' . $originalFilename . '" not found');
 		
+		// declare variables
+		$originalSize = new ImageInfo();
+		$originalSize->sizeFromFile($originalFilename);
+		$requestedSize = new ImageInfo($width, $height);
+		$maxSize = new ImageInfo($maxwidth, $maxheight);
+		$finalSize = new ImageInfo();
+		$imageCopySource = new ImageInfo();
 		
-		// see if we already have this file at stock.
-		$resizedFilename = $this->getResizedImageFileName($originalFilename, $width, $height);
-		if ($resizedFilename === false)
+		// calculate final dimensions
+		$this->calculateSizes($originalSize, $requestedSize, $maxSize, $finalSize, $imageCopySource, ($allowgrow == 1));
+		
+		// if same dimensions, send the original image
+		if ($finalSize->sameAs($originalSize))
 		{
-			Yii::log('Failed getting new image filename for "' . $originalFilename . '"', 'error', 'ImagesController');
+			Yii::log('Final dimensions same as original. Redirecting to original source: "' . $src . '"', 'info', 'ImagesController');
 			header('Location: ' . $src);
 			return;
 		}
 		
+		// get original timestamp to compare and/or save
 		$originalTimestamp = $this->getFileTimestamp($originalFilename);
 		if ($originalTimestamp === false)
 		{
@@ -50,46 +51,38 @@ class ImagesController extends Controller
 			return;
 		}
 		
-		// see if timestamps match, so we send the resized one.
+		// see if we have an image and if we can use it.
+		$resizedFilename = $this->getResizedImageFileName($originalFilename, $finalSize);
+		$resizedUrl = $this->filenameToUrl($resizedFilename);
 		if (file_exists($resizedFilename))
 		{
-			$resizedTimestamp = $this->getFileTimestamp($resizedFilename);
-			if ($resizedTimestamp === false)
+			if ($this->getFileTimestamp($resizedFilename) == $originalTimestamp)
 			{
-				Yii::log('Failed getting file timestamp for "' . $resizedFilename . '"', 'error', 'ImagesController');
-				header('Location: ' . $src);
-				return;
-			}
-			
-			if ($originalTimestamp == $resizedTimestamp)
-			{
-				// encode the slashes as well ???
-				$url = $this->filenameToUrl($resizedFilename);
-				Yii::log('Resized file "' . $resizedFilename . '" has same timestamp as original. Redirecting to url "' . $url . '"', 'info', 'ImagesController');
-				header('Location: ' . $url);
+				Yii::log('Found resized file with same timestamp. Redirecting to it: "' . $resizedUrl . '"', 'info', 'ImagesController');
+				header('Location: ' . $resizedUrl);
 				return;
 			}
 		}
 		
-		
 		// so, we want a specific size, there is no resized file, 
 		// or it has different timestamp, hence we need to create it.
-		$image = $this->loadImage($originalFilename);
-		if ($image === false)
+		Yii::log('Will resize image to ' . $finalSize->width . ' x ' . $finalSize->height, 'info', 'ImagesController');
+		$originalImage = $this->loadImage($originalFilename);
+		if ($originalImage === false)
 		{
 			Yii::log('Failed to load image "' . $originalFilename . '"', 'error', 'ImagesController');
 			header('Location: ' . $src);
 			return;
 		}
 		
-		$resizedImage = $this->resizeImage($image, $width, $height);
+		$resizedImage = $this->resizeImage($originalImage, $finalSize, $imageCopySource);
 		if ($resizedImage === false)
 		{
 			Yii::log('Failed to resize image', 'error', 'ImagesController');
 			header('Location: ' . $src);
 			return;
 		}
-		Yii::log('Image resized to ' . $width . ' x ' . $height, 'info', 'ImagesController');
+		//Yii::log('Image resized to ' . $finalSize->width . ' x ' . $finalSize->height, 'info', 'ImagesController');
 
 			
 		if (!$this->saveImage($resizedImage, $resizedFilename))
@@ -108,149 +101,115 @@ class ImagesController extends Controller
 		
 		
 		// finally...
-		$url = $this->filenameToUrl($resizedFilename);
-		Yii::log('Redirecting to newly created resized image: "' . $url . '"', 'info', 'ImagesController');
-		header('Location: ' . $url);
+		Yii::log('Redirecting to newly created resized image: "' . $resizedUrl . '"', 'info', 'ImagesController');
+		header('Location: ' . $resizedUrl);
+	}
+	
+	protected function calculateSizes($originalSize, $requestedSize, $maxSize, $finalSize, $imageCopySource, $allowGrow)
+	{
+		// first, depending on requested size
+		if ($requestedSize->width != 0 && $requestedSize->height != 0)
+		{
+			// both dimensions givend, try to meet them
+			$finalSize->copyFrom($requestedSize);
+		}
+		else if ($requestedSize->width != 0)
+		{
+			// width given, derive height
+			$finalSize->copyFrom($originalSize);
+			$factor = $requestedSize->width / $originalSize->width;
+			$finalSize->resize($factor);
+		}
+		else if ($requestedSize->height != 0)
+		{
+			// height given, derive width
+			$finalSize->copyFrom($originalSize);
+			$factor = $requestedSize->height / $originalSize->height;
+			$finalSize->resize($factor);
+		}
+		else // no requested size
+		{
+			$finalSize->copyFrom($originalSize);
+		}
+		
+		
+		// if now allowed to grow, and we have grown, keep original size
+		if ($allowGrow == false && $finalSize->sizeLargerThan($originalSize))
+		{
+			$finalSize->copyFrom($originalSize);
+		}
+
+			
+		// now, make sure we do not exceed our limits
+		if ($maxSize->width != 0 && $finalSize->width > $maxSize->width)
+		{
+			$factor = $maxSize->width / $finalSize->width;
+			$finalSize->resize($factor);
+		}
+		if ($maxSize->height != 0 && $finalSize->height > $maxSize->height)
+		{
+			$factor = $maxSize->height / $finalSize->height;
+			$finalSize->resize($factor);
+		}
+		
+		
+		// now, derive the source rectangle
+		$originalRatio = $originalSize->getAspectRatio();
+		$finalRatio = $finalSize->getAspectRatio();
+		Yii::log('Original aspect ratio ' . $originalRatio . ', final aspect ratio ' . $finalRatio, 'debug', 'ImagesController');
+		
+		if ($finalRatio == $originalRatio)
+		{
+			// same aspect ratio, we take the whole picture
+			$imageCopySource->copyFrom($originalSize);
+			$imageCopySource->x = 0;
+			$imageCopySource->y = 0;
+		}
+		else if ($finalRatio < $originalRatio)
+		{
+			// final image is narrower than original - we take the whole height
+			
+			$imageCopySource->y = 0;
+			$imageCopySource->height = $originalSize->height;
+			
+			$imageCopySource->width = round($originalSize->width * ($finalRatio / $originalRatio));
+			$imageCopySource->x = floor($originalSize->width - $imageCopySource->width) / 2;
+		}
+		else if ($finalRatio > $originalRatio)
+		{
+			// final image is wider than original - we take the whole width
+			// final image is narrower than original - we take the whole height
+			$imageCopySource->x = 0;
+			$imageCopySource->width = $originalSize->width;
+			
+			$imageCopySource->height = round($originalSize->height * ($originalRatio / $finalRatio));
+			$imageCopySource->y = floor($originalSize->height - $imageCopySource->height) / 2;
+		}
+		
+		Yii::log('ImageCopySource: Width=' . $imageCopySource->width . ', Height=' . $imageCopySource->height . ', X=' . $imageCopySource->x . ', Y=' . $imageCopySource->y, 'debug', 'ImagesController');
 	}
 	
 	
 	
-	protected function getResizedImageFileName($filename, $width = '', $height = '')
+	
+	
+	protected function getResizedImageFileName($filename, $size)
 	{
-		// if no dimensions given, return original filename
-		if ($width == '' && $height == '')
-			return $filename;
-		
-		// derive one dimension if one is not given
-		$size = getimagesize($filename);
-		if ($size === false)
-			return false;
-		
-		$original_width = $size[0];
-		$original_height = $size[1];
-		
-		if ($width == '')
-			$width = ceil($original_width * ($height / $original_height));
-
-		if ($height == '')
-			$height = ceil($original_height * ($width / $original_width));
-
 		$extension = pathinfo($filename, PATHINFO_EXTENSION);
 		$before_extension = substr($filename, 0, strlen($filename) - strlen($extension) - 1);
-		return $before_extension . '.' . $width . 'x' . $height . '.' . $extension;
+		return $before_extension . '.' . $size->width . 'x' . $size->height . '.' . $extension;
 	}
 	
-	protected function resizeImage($originalImage, &$width, &$height)
+	protected function resizeImage($originalImage, $finalSize, $imageCopySource)
 	{
-		$original_width = imagesx($originalImage);
-		$original_height = imagesy($originalImage);
-		$requested_width = $width;
-		$requested_height = $height;
-		
-		$src_x = 0;
-		$src_y = 0;
-		$src_w = 0;
-		$src_h = 0;
-		$dst_w = 0;
-		$dst_h = 0;
-		
-		$this->calculateDimensions(
-			$original_width, $original_height, 
-			$requested_width, $requested_height, 
-			$src_x, $src_y, $src_w, $src_h, $dst_w, $dst_h);
-		
-		$resizedImage = imageCreateTrueColor($dst_w, $dst_h);
-		imageCopyResampled($resizedImage, $originalImage, 0, 0, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
-		
-		$width = $dst_w;
-		$height = $dst_h;
+		$resizedImage = imageCreateTrueColor($finalSize->width, $finalSize->height);
+		imageCopyResampled($resizedImage, $originalImage, 
+			0, 0, 
+			$imageCopySource->x, $imageCopySource->y, 
+			$finalSize->width, $finalSize->height, 
+			$imageCopySource->width, $imageCopySource->height);
 		
 		return $resizedImage;
-	}
-	
-	protected function calculateDimensions(
-		$original_width, $original_height, 
-		$requested_width, $requested_height, 
-		&$src_x, &$src_y, &$src_w, &$src_h, &$dst_w, &$dst_h)
-	{
-		if ($requested_width == 0 && $requested_height == 0)
-		{
-			// no dimensions, no resizing
-			$src_x = 0;
-			$src_y = 0;
-			$src_w = $original_width;
-			$src_h = $original_height;
-			$dst_w = $original_width;
-			$dst_h = $original_height;
-		}
-		else if ($requested_height != 0 && $requested_width == 0)
-		{
-			// can be any width. resize based on height.
-			$scale_factor = $requested_height / $original_height;
-			
-			$src_x = 0;
-			$src_y = 0;
-			$src_w = $original_width;
-			$src_h = $original_height;
-			$dst_w = ceil($original_width * $scale_factor);
-			$dst_h = $requested_height;
-		}
-		else if ($requested_width != 0 && $requested_height == 0)
-		{
-			// can be any height. resize based on width.
-			$scale_factor = $requested_width / $original_width;
-			
-			$src_x = 0;
-			$src_y = 0;
-			$src_w = $original_width;
-			$src_h = $original_height;
-			$dst_w = $requested_width;
-			$dst_h = ceil($original_height * $scale_factor);
-		}
-		else if ($requested_width != 0 && $requested_height != 0)
-		{
-			// both dimensions given, scale, honouring aspect ratio. caller wants to fill said space.
-			$dst_w = $requested_width;
-			$dst_h = $requested_height;
-			
-			// first, try landscape image, as is usual, scale to width, check height to crop.
-			$scale_factor = $requested_width / $original_width;
-			$resulting_width = ceil($original_width * $scale_factor);
-			$resulting_height = ceil($original_height * $scale_factor);
-			
-			if ($resulting_height > $requested_height)
-			{
-				// good, image is taller than requested. crop some at the top and bottom of it.
-				$src_w = $original_width;
-				$src_h = ceil($requested_height / $scale_factor);
-				$src_x = 0;
-				$src_y = ($original_height - $src_h) / 2;
-			}
-			else if ($resulting_height == $requested_height)
-			{
-				// good, aspect ratio is maintained!
-				$src_w = $original_width;
-				$src_h = $original_height;
-				$src_x = 0;
-				$src_y = 0;
-			}
-			else if ($resulting_height < $requested_height)
-			{
-				// image would not be tall enough, if we resized based on width. 
-				// resize the height, check the width.
-				$scale_factor = $requested_height / $original_height;
-				$resulting_width = ceil($original_width * $scale_factor);
-				$resulting_height = ceil($original_height * $scale_factor);
-				
-				// there is only one case: the image is wider than needed.
-				// it cannot be the same, for if aspect ratio is maintened, we would be in the "elseif" above,
-				// it cannot be narrower, for we would be in the first "if"
-				$src_w = ceil($requested_width / $scale_factor);
-				$src_h = $original_height;
-				$src_x = ($original_width - $src_w) / 2;
-				$src_y = 0;
-			}
-		}
 	}
 	
 	protected function loadImage($filename)
@@ -314,4 +273,65 @@ class ImagesController extends Controller
 		$url = implode('/', $parts);
 		return $url;
 	}
+	
+	
 }
+
+
+class ImageInfo
+{
+	function __construct($width = 0, $height = 0, $x = 0, $y = 0)
+	{
+		$this->width = $width;
+		$this->height = $height;
+		$this->x = $x;
+		$this->y = $y;
+	}
+	
+	public $width;
+	public $height;
+	public $x;
+	public $y;
+
+	function copyFrom(ImageInfo $other)
+	{
+		$this->width = $other->width;
+		$this->height = $other->height;
+		$this->x = $other->x;
+		$this->y = $other->y;
+	}
+	
+	function sizeFromFile($filename)
+	{
+		$size = getimagesize($filename);
+		if ($size === false)
+			return false;
+		
+		$this->width = $size[0];
+		$this->height = $size[1];
+		
+		return true;
+	}
+	
+	function getAspectRatio()
+	{
+		return ($this->height == 0) ? 0 : ($this->width / $this->height);
+	}
+	
+	function resize($factor)
+	{
+		$this->width = round($this->width * $factor);
+		$this->height = round($this->height * $factor);
+	}
+
+	function sameAs(ImageInfo $other)
+	{
+		return ($this->width == $other->width && $this->height == $other->height && $this->x == $other->x && $this->y == $other->y);
+	}
+	
+	function sizeLargerThan(ImageInfo $other)
+	{
+		return ($this->width > $other->width || $this->height > $other->height);
+	}
+}
+
