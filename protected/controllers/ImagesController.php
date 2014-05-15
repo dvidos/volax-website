8c3c0e8746e368d2e4952a7f8f1e4ca8cfd123ca
Dimitris <Dimitris@dimitris4520s>
2014-05-15 16:58:06 +0300

    images controller now saves resized image in new file and redirects

--- protected/controllers/ImagesController.php
+++ protected/controllers/ImagesController.php
@@ -12,58 +12,163 @@ class ImagesController extends Controller
 	 */
 	public function actionShow($src = '', $width = '', $height = '')
 	{
-		// sometime in the future, we should cache the result of the resize to make it fast.
-		// we do not check for "is_file", because sometimes we are asked to resize remote images.
-		//if (!is_file($src))
-		//	throw new CHttpException(404);
-		
+		if ($src == '')
+			throw new CHttpException(400, 'No image SRC given');
+
 		// if nothing given, simply redirect to the file.
 		if (($width == '' && $height == '') || ($width == 0 && $height == 0))
 		{
-			$this->_send_headers($src);
+			Yii::log('No dimensions given, redirecting to original source: "' . $src . '"', 'info', 'ImagesController');
 			header('Location: ' . $src);
-			Yii::app()->end();
+			return;
 		}
-
-		// usually, spaces will be rendered as %20. try to decode here...
-		$src = urldecode($src);
 		
-		// we need to resize..
-		if (!($source_image = $this->_img_load($src)))
-			throw new CHttpException(500, 'Cannot load image "' . $src . '"');
+		// usually, spaces will be rendered as %20. try to decode here...
+		$originalFilename = urldecode($src);
+		if ($originalFilename != $src)
+			Yii::log('Original filename for url "' . $src . '" is "' . $originalFilename . '"', 'info', 'ImagesController');
+			
+		
+		if (!file_exists($originalFilename))
+			throw new CHttpException('File "' . $originalFilename . '" not found');
+		
+		
+		// see if we already have this file at stock.
+		$resizedFilename = $this->getResizedImageFileName($originalFilename, $width, $height);
+		if ($resizedFilename === false)
+		{
+			Yii::log('Failed getting new image filename for "' . $originalFilename . '"', 'error', 'ImagesController');
+			header('Location: ' . $src);
+			return;
+		}
+		
+		$originalTimestamp = $this->getFileTimestamp($originalFilename);
+		if ($originalTimestamp === false)
+		{
+			Yii::log('Failed getting file timestamp for "' . $originalFilename . '"', 'error', 'ImagesController');
+			header('Location: ' . $src);
+			return;
+		}
+		
+		// see if timestamps match, so we send the resized one.
+		if (file_exists($resizedFilename))
+		{
+			$resizedTimestamp = $this->getFileTimestamp($resizedFilename);
+			if ($resizedTimestamp === false)
+			{
+				Yii::log('Failed getting file timestamp for "' . $resizedFilename . '"', 'error', 'ImagesController');
+				header('Location: ' . $src);
+				return;
+			}
+			
+			if ($originalTimestamp == $resizedTimestamp)
+			{
+				// encode the slashes as well ???
+				$url = $this->filenameToUrl($resizedFilename);
+				Yii::log('Resized file "' . $resizedFilename . '" has same timestamp as original. Redirecting to url "' . $url . '"', 'info', 'ImagesController');
+				header('Location: ' . $url);
+				return;
+			}
+		}
+		
+		
+		// so, we want a specific size, there is no resized file, 
+		// or it has different timestamp, hence we need to create it.
+		$image = $this->loadImage($originalFilename);
+		if ($image === false)
+		{
+			Yii::log('Failed to load image "' . $originalFilename . '"', 'error', 'ImagesController');
+			header('Location: ' . $src);
+			return;
+		}
+		
+		$resizedImage = $this->resizeImage($image, $width, $height);
+		if ($resizedImage === false)
+		{
+			Yii::log('Failed to resize image', 'error', 'ImagesController');
+			header('Location: ' . $src);
+			return;
+		}
+		Yii::log('Image resized to ' . $width . ' x ' . $height, 'info', 'ImagesController');
 
-		$original_width = imagesx($source_image);
-		$original_height = imagesy($source_image);
+			
+		if (!$this->saveImage($resizedImage, $resizedFilename))
+		{
+			Yii::log('Failed to save resized image: "' . $resizedFilename . '"', 'error', 'ImagesController');
+			header('Location: ' . $src);
+			return;
+		}
+		
+		if (!$this->setFileTimestamp($resizedFilename, $originalTimestamp))
+		{
+			Yii::log('Failed to set resized image file timestamp: "' . $resizedFilename . '"', 'error', 'ImagesController');
+			header('Location: ' . $src);
+			return;
+		}
+		
+		
+		// finally...
+		$url = $this->filenameToUrl($resizedFilename);
+		Yii::log('Redirecting to newly created resized image: "' . $url . '"', 'info', 'ImagesController');
+		header('Location: ' . $url);
+	}
+	
+	
+	
+	protected function getResizedImageFileName($filename, $width = '', $height = '')
+	{
+		// if no dimensions given, return original filename
+		if ($width == '' && $height == '')
+			return $filename;
+		
+		// derive one dimension if one is not given
+		$size = getimagesize($filename);
+		if ($size === false)
+			return false;
+		
+		$original_width = $size[0];
+		$original_height = $size[1];
+		
+		if ($width == '')
+			$width = ceil($original_width * ($height / $original_height));
+
+		if ($height == '')
+			$height = ceil($original_height * ($width / $original_width));
+
+		$extension = pathinfo($filename, PATHINFO_EXTENSION);
+		$before_extension = substr($filename, 0, strlen($filename) - strlen($extension) - 1);
+		return $before_extension . '.' . $width . 'x' . $height . '.' . $extension;
+	}
+	
+	protected function resizeImage($originalImage, &$width, &$height)
+	{
+		$original_width = imagesx($originalImage);
+		$original_height = imagesy($originalImage);
 		$requested_width = $width;
 		$requested_height = $height;
 		
 		$src_x = 0;
 		$src_y = 0;
-		$dst_x = 0;
-		$dst_y = 0;
-		
 		$src_w = 0;
 		$src_h = 0;
 		$dst_w = 0;
 		$dst_h = 0;
 		
-		$this->_calculate_dimensions(
+		$this->calculateDimensions(
 			$original_width, $original_height, 
 			$requested_width, $requested_height, 
 			$src_x, $src_y, $src_w, $src_h, $dst_w, $dst_h);
 		
-		//die('initial image width is ' . $original_width . ', requested width is ' . $width . ', new image width is ' . $new_img_width . ', src_x is ' . $src_x);
+		$resizedImage = imageCreateTrueColor($dst_w, $dst_h);
+		imageCopyResampled($resizedImage, $originalImage, 0, 0, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
 		
-		$target_image = imageCreateTrueColor($dst_w, $dst_h);
-		imageCopyResampled($target_image, $source_image, 0, 0, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h);
+		$width = $dst_w;
+		$height = $dst_h;
 		
-		
-		// send it.
-		$this->_send_headers($src);
-		$this->_img_send($target_image, $src);
+		return $resizedImage;
 	}
-
-	protected function _calculate_dimensions(
+	
+	protected function calculateDimensions(
 		$original_width, $original_height, 
 		$requested_width, $requested_height, 
 		&$src_x, &$src_y, &$src_w, &$src_h, &$dst_w, &$dst_h)
@@ -87,7 +192,7 @@ class ImagesController extends Controller
 			$src_y = 0;
 			$src_w = $original_width;
 			$src_h = $original_height;
-			$dst_w = $original_width * $scale_factor;
+			$dst_w = ceil($original_width * $scale_factor);
 			$dst_h = $requested_height;
 		}
 		else if ($requested_width != 0 && $requested_height == 0)
@@ -100,7 +205,7 @@ class ImagesController extends Controller
 			$src_w = $original_width;
 			$src_h = $original_height;
 			$dst_w = $requested_width;
-			$dst_h = $original_height * $scale_factor;
+			$dst_h = ceil($original_height * $scale_factor);
 		}
 		else if ($requested_width != 0 && $requested_height != 0)
 		{
@@ -148,71 +253,65 @@ class ImagesController extends Controller
 		}
 	}
 	
-	protected function _send_headers($filename)
+	protected function loadImage($filename)
 	{
-		$extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
-		if ($extension == "jpg" || $extension == "jpeg")
-		{
-			header("Content-type: image/jpeg");
-		}
-		else if($extension == "png")
-		{
-			header("Content-type: image/png");
-		}
-		elseif($extension == "gif")
-		{
-			header("Content-type: image/gif");
-		}
-	}
-	
-	protected function _img_load($source_file)
-	{
+		Yii::log('Loading image "' . $filename . '"', 'info');
 		$image = false;
-		$extension = strtolower(pathinfo($source_file, PATHINFO_EXTENSION));
+		$extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 		
 		if ($extension == "jpg" || $extension == "jpeg")
-		{
-			$image = imageCreateFromJPEG($source_file);
-		}	
+			$image = imageCreateFromJPEG($filename);
 		elseif ($extension == "png")
-		{
-			$image = imageCreateFromPNG($source_file);
-		}	
+			$image = imageCreateFromPNG($filename);
 		elseif ($extension == "gif")
-		{
-			$image = imageCreateFromGIF($source_file);
-		}
+			$image = imageCreateFromGIF($filename);
 		
 		if (!$image)
-		{
-			// log_warning('_img_load(): could not load file "' . $source_file . '"');
-			return false;
-		}
+			Yii::log('Failed loading image "' . $filename . '"', 'error');
 		
 		return $image;
 	}
 
-	protected function _img_send($image, $source_file)
+	protected function saveImage($img, $filename)
 	{
+		Yii::log('Saving image to "' . $filename . '"', 'info');
 		$saved = false;
-		$extension = strtolower(pathinfo($source_file, PATHINFO_EXTENSION));
+		$extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 		
 		if ($extension == "jpg" || $extension == "jpeg")
-		{
-			$jpeg_quality = 90;
-			$saved = imageJPEG($image);
-		}
-		else if($extension == "png")
-		{
-			$saved = imagePNG($image);
-		}
-		// Save GIF
-		elseif($extension == "gif")
-		{
-			$saved = imageGIF($image);
-		}
+			$saved = imagejpeg($img, $filename);
+		elseif ($extension == "png")
+			$saved = imagepng($img, $filename);
+		elseif ($extension == "gif")
+			$saved = imagegif($img, $filename);
 		
-		return true;
+		if (!$saved)
+			Yii::log('Failed saving image "' . $filename . '"', 'error');
+		
+		return $saved;
 	}
 	
+	protected function getFileTimestamp($filename)
+	{
+		return filemtime($filename);
+	}
+	
+	protected function setFileTimestamp($filename, $timestamp)
+	{
+		if (!file_exists($filename))
+			return false;
+		
+		return touch($filename, $timestamp);
+	}
+	
+	protected function filenameToUrl($filename)
+	{
+		// urlencode() will not work. "uploads/tinos.jpg" becomes "uploads%2F/tinos.jpg"
+		$parts = explode('/', $filename);
+		for ($i = 0; $i < count($parts); $i++)
+			$parts[$i] = rawurlencode($parts[$i]);
+			
+		$url = implode('/', $parts);
+		return $url;
+	}
 }
