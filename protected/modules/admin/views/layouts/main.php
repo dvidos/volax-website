b5242bbeb934e538660bb923033e3f274efb4b65
Dimitris <Dimitris@dimitris4520s>
2014-05-15 16:57:37 +0300

    minor improvement

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -14,7 +14,7 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?> - Admin</div>
+		<div id="logo">Διαχειριστής <?php echo CHtml::encode(Yii::app()->name); ?> - <?php echo CHtml::encode(Yii::app()->user->name); ?></div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
--- protected/modules/author/views/layouts/main.php
+++ protected/modules/author/views/layouts/main.php
@@ -14,7 +14,7 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?> - Συντάκτης</div>
+		<div id="logo">Συντάκτης <?php echo CHtml::encode(Yii::app()->name); ?> - <?php echo CHtml::encode(Yii::app()->user->name); ?></div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
