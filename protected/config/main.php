0332b63da3f24a4461494465c7942bfa75e52726
Dimitris <Dimitris@dimitris4520s>
2015-06-21 20:24:34 +0300

    Main title changed to "Volax" in greek instead of "volax.gr"

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'volax.gr',
+	'name'=>'Βωλάξ',
 	'language'=>'el',
 
 	// preloading 'log' component
