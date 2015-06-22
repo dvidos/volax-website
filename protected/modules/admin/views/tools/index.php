e4088d37fe40705810cc71f37da3eb06b5639424
Dimitris <Dimitris@dimitris4520s>
2015-06-22 20:52:20 +0300

    xml sitemap improved and tested (and submitted)!

--- protected/modules/admin/views/tools/index.php
+++ protected/modules/admin/views/tools/index.php
@@ -1,37 +1,53 @@
 <h1>Εργαλεία</h1>
 
-<table>
-<tr><td width="33%">
+<table><tr><td width="33%">
+	
+	
+	<h3>Xml Sitemap</h3>
+	
 	<p>Εδώ μπορείτε να παράξετε ένα αρχείο 
 	<?php echo CHtml::link('sitemaps', 'https://support.google.com/webmasters/answer/156184?hl=en', array('target'=>'_blank')); ?>
-	για καλύτερη αναζήτηση από την Google και άλλες μηχανές αναζήτησης</p>
+	για καλύτερη αναζήτηση από την Google και άλλες μηχανές αναζήτησης, όπως προτείνεται από τα 
+	<?php echo CHtml::link('Google Webmaster Tools', 'https://www.google.com/webmasters/tools', array('target'=>'_blank')); ?>.</p>
 	
+	<p>Το αρχείο θα είναι το <?php echo $xml_sitemap_filename; ?> 
+	και μπορείτε να το δείτε στο <?php echo CHtml::link($xml_sitemap_url, $xml_sitemap_url); ?></p>
+	
+	<?php
+		if(Yii::app()->user->hasFlash('xmlSitemap'))
+		{
+			echo CHtml::tag('div', array('class'=>'flash-success'), CHtml::encode(Yii::app()->user->getFlash('xmlSitemap')));
+			$js = '$(document).ready(function(){ setTimeout(function() { $(".flash-success").slideUp(); }, 4000); });';
+			echo CHtml::tag('script', array(), $js);
+		}
+	?>
 	<p><?php
 		echo CHtml::button('Παραγωγή xml sitemap', array(
 			'onClick'=>'window.location = "' . $this->createUrl('/admin/tools/xmlSitemap') . '";',
 		)) . '<br />';;
 	?></p>
 	
-	<p>Για να στείλετε το αρχείο στην Google, επισκεφτείτε το 
-	<?php echo CHtml::link('Google Webmaster Tools', 'https://www.google.com/webmasters/tools', array('target'=>'_blank')); ?>
-	</p>
+	
+	
+	
 	
 	
 	
 	
 </td><td width="33%">
 
-b
+	
+	
+	&nbsp;
 
 
 
 </td><td width="33%">
 
-c
+	&nbsp;
 
 
 
-</td></tr>
-</table>
+</td></tr></table>
 
 
--- protected/modules/admin/views/tools/xmlSitemap.php
+++ protected/modules/admin/views/tools/xmlSitemap.php
@@ -1,57 +1,69 @@
 <?php 
-	$filename = 'Volax.gr Xml Sitemap.xml';
-	header('Content-type: application/xml'); 
-	header("Content-disposition: attachment; filename=\"$filename\"");
-?><?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
-<?php
-/*
-
-Tag				Required?	Description
------------		----------	---------------------------------------
-<urlset>		Required	Encloses all information about the set of URLs included in the sitemap.
-<url>			Required	Encloses all information about a specific URL.
-<loc>			Required	Specifies the URL. For images and video, specifies the landing page (aka play page).
-<lastmod>		Optional	Shows the date the URL was last modified, in YYYY-MM-DDThh:mmTZD format (time value is optional).
-<changefreq>	Optional	Provides a hint about how frequently the page is likely to change. Valid values are:
-							- always. Use for pages that change every time they are accessed.
-							- hourly
-							- daily
-							- weekly
-							- monthly
-							- yearly
-							- never. Use this value for archived URLs.
-<priority>		Optional	Describes the priority of a URL relative to all the other URLs on the site. 
-							This priority can range from 1.0 (extremely important) to 0.1 (not important at all).
-							Note that the priority tag does not affect your site ranking in Google search results. 
-							Priority values are only considered relative to other pages on your site so, 
-							assigning a high priority (or specifying the same priority for all URLs) 
-							will not boost your entire site search ranking.
-*/
-
-
-	echo "\t<url>\r\n";
-	echo "\t\t" . CHtml::tag('loc', array(), $this->createAbsoluteUrl('/')) . "\r\n";
-	echo "\t\t" . CHtml::tag('changefreq', array(), 'daily') . "\r\n";
-	echo "\t\t" . CHtml::tag('priority', array(), '0.8') . "\r\n";
-	echo "\t</url>\r\n";
-		
-	foreach ($posts as $post)
+	//header('Content-type: application/xml'); 
+	//header('Content-type: text/plain'); 
+	//$filename = 'Volax.gr Xml Sitemap.xml';
+	//header("Content-disposition: attachment; filename=\"$filename\"");
+	//foreach ($posts as $post)
+	//	echo 'Post ' . $post->title . '<br />';
+	//foreach ($categories as $category)
+	//	echo 'Category ' . $category->title . '<br />';
+	
+	
+	function formatUrl($loc, $changefreq, $priority, $lastmod = 0)
 	{
-		echo "\t<url>\r\n";
-		echo "\t\t" . CHtml::tag('loc', array(), $post->getUrl(true)) . "\r\n";
-		echo "\t\t" . CHtml::tag('priority', array(), '0.5') . "\r\n";
-		echo "\t\t" . CHtml::tag('lastmod', array(), date('Y-m-d\TH:iP', $post->update_time)) . "\r\n";
-		echo "\t</url>\r\n";
+		/*
+		Tag				Required?	Description
+		-----------		----------	---------------------------------------
+		<urlset>		Required	Encloses all information about the set of URLs included in the sitemap.
+		<url>			Required	Encloses all information about a specific URL.
+		<loc>			Required	Specifies the URL. For images and video, specifies the landing page (aka play page).
+		<lastmod>		Optional	Shows the date the URL was last modified, in YYYY-MM-DDThh:mmTZD format (time value is optional).
+		<changefreq>	Optional	Provides a hint about how frequently the page is likely to change. Valid values are:
+									- always. Use for pages that change every time they are accessed.
+									- hourly, daily, weekly, monthly, yearly
+									- never. Use this value for archived URLs.
+		<priority>		Optional	Describes the priority of a URL relative to all the other URLs on the site. 
+									This priority can range from 1.0 (extremely important) to 0.1 (not important at all).
+									Note that the priority tag does not affect your site ranking in Google search results. 
+									Priority values are only considered relative to other pages on your site so, 
+									assigning a high priority (or specifying the same priority for all URLs) 
+									will not boost your entire site search ranking.
+		*/
+		
+		$html = '';
+		$html .= "\t<url>\r\n";
+		$html .= "\t\t" . CHtml::tag('loc', array(), $loc) . "\r\n";
+		$html .= "\t\t" . CHtml::tag('changefreq', array(), $changefreq) . "\r\n";
+		$html .= "\t\t" . CHtml::tag('priority', array(), $priority) . "\r\n";
+		
+		if ($lastmod != 0)
+			$html .= "\t\t" . CHtml::tag('lastmod', array(), date('Y-m-d\TH:iP', $lastmod)) . "\r\n";
+			
+		$html .= "\t</url>\r\n";
+		
+		return $html;
 	}
 	
-	foreach ($categories as $category)
+	function generateXml($posts, $categories)
 	{
-		echo "\t<url>\r\n";
-		echo "\t\t" . CHtml::tag('loc', array(), $category->getUrl(true)) . "\r\n";
-		echo "\t\t" . CHtml::tag('changefreq', array(), 'weekly') . "\r\n";
-		echo "\t\t" . CHtml::tag('priority', array(), '0.6') . "\r\n";
-		echo "\t</url>\r\n";
+		$xml = '';
+		
+		$xml .= "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n";
+		$xml .= "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\r\n";
+
+		$xml .= formatUrl(Yii::app()->createAbsoluteUrl('/'), 'daily', '0.8', 0);
+		
+		foreach ($posts as $post)
+			$xml .= formatUrl($post->getUrl(true), 'weekly', '0.5', $post->update_time);
+			
+		foreach ($categories as $category)
+			$xml .= formatUrl($category->getUrl(true), 'weekly', '0.6');
+	
+		$xml .= "</urlset>\r\n";
+		
+		return $xml;
 	}
-?>
-</urlset>
+	
+	echo generateXml($posts, $categories);
+	
+	
