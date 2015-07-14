e1576f1f16f9ae3c63f1970ac8c66c1660dfa84a
Dimitris <Dimitris@dimitris4520s>
2015-07-14 18:18:55 +0300

    mixed language now with automatic correction...

--- protected/components/validators/NoMixedLangValidator.php
+++ protected/components/validators/NoMixedLangValidator.php
@@ -15,7 +15,24 @@ class NoMixedLangValidator extends CValidator
 	{
 		$value = $object->$attribute;
 		if (preg_match($this->regexp, $value))
-			$this->addError($object, $attribute, 'Βρέθηκαν λέξεις με μεικτά ελληνικά-αγγλικά (' . mb_strtolower($value, 'utf-8') . ')');
+		{
+			// we try to fix the value automatically for example "toπωnymia" to "τοπωνυμια".
+			$greek = $this->forceGreekCapitals2($value);
+			$object->$attribute = $greek;
+			Yii::log('Το "' . mb_strtolower($value, 'utf-8') . '" μετατράπηκε σε "' . mb_strtolower($greek, 'utf-8') . '" (utf-8 encoding)', 'warning');
+			
+			// check again
+			$value = $object->$attribute;
+			if (preg_match($this->regexp, $value))
+				$this->addError($object, $attribute, 'Βρέθηκαν λέξεις με μεικτά ελληνικά-αγγλικά (' . mb_strtolower($value, 'utf-8') . ')');
+			}
+	}
+	
+	function forceGreekCapitals2($value)
+	{
+		$en = array('E', 'T', 'Y', 'I', 'O', 'P', 'A', 'H', 'K', 'Z', 'X', 'B', 'N', 'M');
+		$gr = array('Ε', 'Τ', 'Υ', 'Ι', 'Ο', 'Ρ', 'Α', 'Η', 'Κ', 'Ζ', 'Χ', 'Β', 'Ν', 'Μ');
+		return str_replace($en, $gr, $value);
 	}
 }
 
