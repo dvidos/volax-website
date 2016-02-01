c660dea79231abee7f1eaa8dd4aaa69039c74436
Dimitris Vidos <devnull@localhost>
2016-02-01 22:46:43 +0200

    Post editing now has a comments tab

--- protected/modules/admin/views/posts/_tabs.php
+++ protected/modules/admin/views/posts/_tabs.php
@@ -6,6 +6,7 @@
 			array('label'=>'Διόρθωση', 'url'=>array('/admin/posts/update', 'id'=>$model->id)),
 			array('label'=>'Σημειώσεις', 'url'=>array('/admin/posts/discuss', 'id'=>$model->id)),
 			array('label'=>'Ιστορικό (' . $model->revisionCount . ')', 'url'=>array('/admin/posts/history', 'id'=>$model->id)),
+			array('label'=>'Σχόλια (' . $model->commentCount . ')', 'url'=>array('/admin/comments/index', 'post_id'=>$model->id)),
 			array('label'=>'Πληροφορίες', 'url'=>array('/admin/posts/info', 'id'=>$model->id)),
 			array('label'=>'Επισκόπιση', 'url'=>array('/post/view', 'id'=>$model->id), 'linkOptions'=>array('target'=>'_blank')),
 		),
